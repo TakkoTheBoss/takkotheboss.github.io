7b1493407681afd4b959a28c09c3b4d5a82c9f99
Defiant_Networks_Inc <mike.curnow@defiant-networks.com>
2023-09-08 10:52:30 -0400

    fix site url and base path

--- src/site_config.ts
+++ src/site_config.ts
@@ -3,7 +3,7 @@
 // Metadata, SEO, and Social
 export const SITE_TITLE = "Manual Theme"
 export const SITE_DESCRIPTION = "A documentation template for Astro"
-export const SITE_URL = "https://takkotheboss.github.io/hachilang/"
+export const SITE_URL = "https://takkotheboss.github.io/"
 export const SITE_DEFAULT_OG_IMAGE = "/assets/og-image.png"
 
 // Docs Sidebar
