e07f59084a2d6adc4bf7bbfe80cf77625cccf2de
Defiant_Networks_Inc <mike.curnow@defiant-networks.com>
2024-10-25 01:44:20 -0400

    Testing index.astro for a minute

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { SITE_URL } from './src/site_config';
-import fs from 'fs'; // Import fs for file operations
+import fs from 'fs';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,19 +10,30 @@ export default defineConfig({
   base: '',
   integrations: [tailwind(), sitemap()],
   vite: {
-    plugins: [rawFonts(['.ttf'])],
-    optimizeDeps: { exclude: ['@resvg/resvg-js'] },
     build: {
       rollupOptions: {
-        external: ['shiki/themes/hc_light.json'], // Added rollupOptions here
+        external: ['shiki/themes/hc_light.json'],
       },
     },
+    plugins: [rawFonts(['.ttf'])],
+    optimizeDeps: { exclude: ['@resvg/resvg-js'] },
   },
 });
 
 function rawFonts(ext) {
   return {
     name: 'vite-plugin-raw-fonts',
+    transform(_, id) {
+      if (ext.some(e => id.endsWith(e))) {
+        const buffer = fs.readFileSync(id);
+        return {
+          code: `export default ${JSON.stringify(buffer)}`,
+          map: null
+        };
+      }
+    }
+  };
+}
     transform(_, id) {
       if (ext.some(e => id.endsWith(e))) {
         const buffer = fs.readFileSync(id);
